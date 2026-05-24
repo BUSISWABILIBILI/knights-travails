fcfffa193241c4b106ade5d3de8c8a7a8a65d097
Busiswa Bili-bili <nellybusiswa4@gmail.com>
2026-05-24 22:12:32 +0200

    feat: get valid knight moves

--- script.js
+++ script.js
@@ -15,6 +15,18 @@ function isValidPosition(position) {
   return x >= 0 && x <= 7 && y >= 0 && y <= 7;
 }
 
+function getValidMoves(position) {
+  const [x, y] = position;
+
+  return knightDirections
+    .map(([moveX, moveY]) => [x + moveX, y + moveY])
+    .filter(isValidPosition);
+}
+
 console.log(isValidPosition([0, 0]));
 console.log(isValidPosition([7, 7]));
 console.log(isValidPosition([8, 2]));
+
+console.log(getValidMoves([0, 0]));
+console.log(getValidMoves([3, 3]));
+console.log(getValidMoves([7, 7]));
